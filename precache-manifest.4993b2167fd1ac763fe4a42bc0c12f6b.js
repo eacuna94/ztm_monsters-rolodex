5a8c3bf7034bed1909d217d2e85565b4e6732a4a
Edgar Acuna <acunamatata.ea@gmail.com>
2022-02-21 11:28:20 -0500

    Updates

---
+++ precache-manifest.4993b2167fd1ac763fe4a42bc0c12f6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232a6c8dd59f4560a97336dc8d01c036",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "f3706707e4a127bf1685",
+    "url": "/monsters-rolodex-complete/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex-complete/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-complete/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3706707e4a127bf1685",
+    "url": "/monsters-rolodex-complete/static/js/main.4aed0176.chunk.js"
+  },
+  {
+    "revision": "4b601965c2149392e1f2",
+    "url": "/monsters-rolodex-complete/static/js/runtime-main.b3c2bfd0.js"
+  }
+]);
